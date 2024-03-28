70ab77d62ee1f91cbc9cba5c9e4d9e26d3b033e6
Blaer <blaerzhang@gmail.com>
2024-03-28 01:32:36 -0400

    Finished Give Card Animation

--- Pre-GDC-prototype-addiction/Assets/Scripts/Manager/GiveCardTemp.cs
+++ Pre-GDC-prototype-addiction/Assets/Scripts/Manager/GiveCardTemp.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GiveCardTemp : MonoBehaviour
 {
+    public ScratchCardGenerator generator;
+    public GameObject hand;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,21 @@ public class GiveCardTemp : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void GiveCard()
+    {
+        GameObject card = GameObject.Find("newScratchCard");
+        GameManager.Instance.GetComponent<ResourceManager>().PlayerGold += (int)generator.currentCardPrize;
+        Sequence giveAnimation = DOTween.Sequence();
+        giveAnimation
+            .Append(card.transform.DOMoveY(card.transform.position.y + 1, 0.5f))
+            .Append(hand.transform.DOMoveY(card.transform.position.y + 2, 1f))
+            .AppendInterval(0.5f)
+            .Append(hand.transform.DOMoveY(9, 1f))
+            .Insert(2, card.transform.DOMoveY(9, 1f))
+            .OnComplete((() => { SceneManager.LoadScene("Buy Card"); }));
+
+        giveAnimation.Play();
     }
 }
