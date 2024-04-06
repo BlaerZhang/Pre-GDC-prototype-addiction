a461222ebdd7af355d7f9c9f720a7c5f3552a9e9
Ziggy-dst <542335894@qq.com>
2024-04-06 17:16:20 -0400

    added custom event system, added metaphysics system

--- Pre-GDC-prototype-addiction/Assets/Scripts/MetaphysicsSystem/TrackingStats.cs
+++
@@ -1,7 +0,0 @@
-namespace MetaphysicsSystem
-{
-    public enum TrackingStats
-    {
-        
-    }
-}
---
+++ Pre-GDC-prototype-addiction/Assets/_Scripts/MetaphysicsSystem/RequirementComparer.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using MetaphysicsSystem;
+using UnityEngine;
+
+public class RequirementComparer : MonoBehaviour
+{
+    public List<ScriptableMetaphysics> metaphysicsList;
+
+    private void OnEnable()
+    {
+        StatsTracker.onAfterValueChanged += CheckIfTriggerResponseEvent;
+    }
+
+    private void OnDisable()
+    {
+        StatsTracker.onAfterValueChanged -= CheckIfTriggerResponseEvent;
+    }
+
+    /// <summary>
+    /// check if the metaphysics triggered after relevant value changes
+    /// </summary>
+    private void CheckIfTriggerResponseEvent(string changedVariableName, float variableValue)
+    {
+        print("CheckIfTriggerResponseEvent");
+        foreach (var metaPhysics in metaphysicsList)
+        {
+            int matchedNum = 0;
+            // if contains the variable that changed, means possibly the requirement would be met
+            if (metaPhysics.metaphysicsRequirement.ContainsKey(changedVariableName))
+            {
+                // check if the variable change whether could trigger the metaphysics effect
+                foreach (var requirement in metaPhysics.metaphysicsRequirement)
+                {
+                    // change the match status, if this is the one that changes
+                    if (requirement.Key.Equals(changedVariableName))
+                    {
+                        requirement.Value.isMatched = CompareRequirements(variableValue, requirement.Value.value, requirement.Value.comparator);
+                    }
+
+                    // if the requirement is matched, add the number of requirements matched
+                    if (requirement.Value.isMatched)
+                    {
+                        matchedNum++;
+                    }
+                }
+
+                if (matchedNum == metaPhysics.metaphysicsRequirement.Count)
+                {
+                    // trigger response event
+                    metaPhysics.response.Raise();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    ///  compare two floats
+    /// </summary>
+    /// <param name="currentVariable"></param>
+    /// <param name="comparator"></param>
+    /// <param name="requiredValue"></param>
+    /// <returns>return true if meets the requirement</returns>
+    private bool CompareRequirements(float currentVariable, float requiredValue, Comparator comparator)
+    {
+        switch (comparator)
+        {
+            case Comparator.Equal:
+                if (Mathf.Approximately(requiredValue, currentVariable)) return true;
+                break;
+            case Comparator.Large:
+                if (!Mathf.Approximately(requiredValue, currentVariable) & currentVariable > requiredValue) return true;
+                break;
+            case Comparator.Smaller:
+                if (!Mathf.Approximately(requiredValue, currentVariable) & currentVariable < requiredValue) return true;
+                break;
+            case Comparator.LargeEqual:
+                if (Mathf.Approximately(requiredValue, currentVariable) || currentVariable > requiredValue) return true;
+                break;
+            case Comparator.SmallerEqual:
+                if (Mathf.Approximately(requiredValue, currentVariable) || currentVariable < requiredValue) return true;
+                break;
+        }
+
+        return false;
+    }
+}
