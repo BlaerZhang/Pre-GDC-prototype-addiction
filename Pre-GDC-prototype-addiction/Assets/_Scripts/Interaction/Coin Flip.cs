dc4dbce0953394718c89ac5d49ca997e8f69e6b2
Blaer Zhang <zhangyueyang0022@126.com>
2024-04-10 20:32:00 -0400

    Dotween Animated Coin Flip

--- Pre-GDC-prototype-addiction/Assets/_Scripts/Interaction/Coin Flip.cs
+++ Pre-GDC-prototype-addiction/Assets/_Scripts/Interaction/Coin Flip.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class CoinFlip : MonoBehaviour
 {
     private Rigidbody rigidbody;
+    private bool isFace = true;
+    private bool isFlipping = false;
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F) && !isFlipping)
         {
-            rigidbody.AddForceAtPosition(new Vector3(0,0,Random.Range(-5,-10)),new Vector2(Random.Range(-1,1),Random.Range(-1,1)),ForceMode.Impulse);
+            isFlipping = true;
+            
+            int flipAngle = Random.Range(5, 7) * 180;
+            isFace = ((flipAngle / 180) % 2 == 0) == isFace ? isFace : !isFace;
+            
+            Sequence flipSequence = DOTween.Sequence();
+            flipSequence
+                .Append(transform.DOLocalRotate(new Vector3(0, flipAngle, 0), 1f, RotateMode.FastBeyond360).SetEase(Ease.InQuad))
+                .Insert(0, transform.DOScaleX(1.5f, 0.5f).SetEase(Ease.OutQuad).SetLoops(2, LoopType.Yoyo))
+                .Insert(0, transform.DOScaleY(1.5f, 0.5f).SetEase(Ease.OutQuad).SetLoops(2, LoopType.Yoyo))
+                .OnComplete((() => { isFlipping = false; }));
+
+            flipSequence.Play();
         }
     }
 }
