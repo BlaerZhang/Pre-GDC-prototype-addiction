01d4190e504a35334adf771ad0bb185abd9c2dee
Blaer Zhang <zhangyueyang0022@126.com>
2024-04-25 17:57:06 -0400

    Clock is now split and animated

--- Pre-GDC-prototype-addiction/Assets/_Scripts/Interaction/Beta/Clock.cs
+++ Pre-GDC-prototype-addiction/Assets/_Scripts/Interaction/Beta/Clock.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Resources;
+using DG.Tweening;
 using Manager;
 using TMPro;
 using UnityEngine;
@@ -9,8 +10,13 @@ using ResourceManager = Manager.ResourceManager;
 
 public class Clock : MonoBehaviour
 {
-    public TextMeshProUGUI timeText;
-    
+    public TextMeshProUGUI hourText;
+    public TextMeshProUGUI colun;
+    public TextMeshProUGUI minuteText;
+    private DateTime displayTime;
+    private int displayHour;
+    private int displayMinute;
+
     private void OnEnable()
     {
         ResourceManager.OnTimeChanged += UpdateClock;
@@ -20,9 +26,25 @@ public class Clock : MonoBehaviour
     {
         ResourceManager.OnTimeChanged -= UpdateClock;
     }
-    
+
+    private void Start()
+    {
+        displayTime = GameManager.Instance.resourceManager.CurrentTime;
+        displayHour = displayTime.Hour;
+        displayMinute = displayTime.Minute;
+    }
+
     private void UpdateClock(DateTime newTime)
     {
-        timeText.text = GameManager.Instance.resourceManager.CurrentTime.ToString("t");
+        displayTime = newTime;
+        DOVirtual.Int(displayMinute, newTime.Minute, 1, value =>
+        {
+            minuteText.text = value.ToString("D2");
+        }).OnComplete(() => { displayMinute = newTime.Minute; });
+
+        DOVirtual.Int(displayHour, newTime.Hour, 1, value =>
+        {
+            hourText.text = value.ToString("D2");
+        }).OnComplete(() => { displayHour = newTime.Hour; });
     }
 }
