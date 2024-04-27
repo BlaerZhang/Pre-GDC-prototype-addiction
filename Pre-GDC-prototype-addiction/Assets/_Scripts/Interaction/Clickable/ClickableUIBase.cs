5b31688690a01584e74c2bb661989bc55b0b7f62
Blaer Zhang <zhangyueyang0022@126.com>
2024-04-27 18:44:55 -0400

    Adjusted some shop visuals

--- Pre-GDC-prototype-addiction/Assets/_Scripts/Interaction/Clickable/ClickableUIBase.cs
+++ Pre-GDC-prototype-addiction/Assets/_Scripts/Interaction/Clickable/ClickableUIBase.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using Manager;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using UnityEngine.EventSystems;
@@ -12,14 +14,16 @@ namespace Interaction.Clickable
         [Header("Size Modifier")]
         [SerializeField] private float hoverSizeModifier = 1.1f;
         [SerializeField] private float pressSizeModifier = 0.8f;
+        private Vector3 originalLocalScale;
 
         [Header("Sound")]
         [SerializeField] private List<AudioClip> hoverSounds = new();
         [SerializeField] private List<AudioClip> pressSounds = new();
         [SerializeField] private List<AudioClip> exitSounds = new();
-
+        
         public virtual void OnPointerEnter(PointerEventData eventData)
         {
+            if (originalLocalScale == Vector3.zero) originalLocalScale = transform.localScale;
             ScaleUpClickable(hoverSizeModifier);
             PlaySound(hoverSounds);
         }
@@ -52,12 +56,12 @@ namespace Interaction.Clickable
 
         private void ScaleUpClickable(float modifier)
         {
-            transform.localScale *= modifier;
+            transform.DOScale(originalLocalScale * modifier, 0.3f).SetEase(Ease.OutElastic);
         }
 
         private void ScaleDownClickable(float modifier)
         {
-            transform.localScale /= modifier;
+            transform.DOScale(originalLocalScale, 0.3f).SetEase(Ease.OutElastic);
         }
     }
 }
