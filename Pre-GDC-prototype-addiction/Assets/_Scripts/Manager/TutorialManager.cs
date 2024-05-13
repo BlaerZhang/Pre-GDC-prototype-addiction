b6eaf5d92a0945a65bf67461665fcc8bd6b36bc8
Blaer Zhang <zhangyueyang0022@126.com>
2024-05-13 18:57:11 -0400

    NEW: Finished first card tutorial

--- Pre-GDC-prototype-addiction/Assets/_Scripts/Manager/TutorialManager.cs
+++ Pre-GDC-prototype-addiction/Assets/_Scripts/Manager/TutorialManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using _Scripts.Interaction.PosterPicking;
 using _Scripts.Manager;
 using _Scripts.PlayerTools.Payphone;
 using Abu;
@@ -15,6 +16,7 @@ public class TutorialManager : MonoBehaviour
     [Title("Opening")] 
     [SerializeField] private AudioClip payphoneRingSound;
     [SerializeField] private AudioClip payphonePickUpSound;
+    [SerializeField] private AudioClip lightOnSound;
     [SerializeField] private Volume payphoneVolume;
     [SerializeField] private GameObject raycastBlocker;
     [SerializeField] private RawImage mask;
@@ -24,27 +26,51 @@ public class TutorialManager : MonoBehaviour
     [SerializeField] private TutorialHighlight moneyHighlight;
     [SerializeField] private List<TutorialHighlight> fruitiePosterHighlights;
     [SerializeField] private List<TutorialHighlight> toolsHighlights;
+
+    [Title("First Card Highlights")] 
+    [SerializeField] private TutorialHighlight prizeGridHighlight;
+    [SerializeField] private GameObject targetGridHighlights;
     
     private StatsTrackingManager statsTrackingManager;
-    private bool isPickedUp = false;
+    private bool isPayphonePickedUp = false;
     private bool isLightOn = false;
+    private bool isCardZoomedForFirstTime = false;
     private GameObject ringAudioTemp;
 
     private void OnEnable()
     {
         PayphoneManager.onSingleLineBegins += Opening;
+        ScratchCardDealer.onToScratchStage += (brand, i, arg3, arg4) =>
+        {
+            if(isCardZoomedForFirstTime) return;
+            isCardZoomedForFirstTime = true;
+            PayphoneManager.onSingleLineBegins += FirstCard;
+            raycastBlocker.SetActive(true);
+            PayphoneManager.onPhoneStateChanged?.Invoke(true);
+            DOVirtual.DelayedCall(1, () => PayphoneManager.onPhoneMessageSent?.Invoke("First Card")).Play();
+        };
     }
 
     private void OnDisable()
     {
         PayphoneManager.onSingleLineBegins -= Opening;
+        ScratchCardDealer.onToScratchStage -= (brand, i, arg3, arg4) =>
+        {
+            if(isCardZoomedForFirstTime) return;
+            isCardZoomedForFirstTime = true;
+            PayphoneManager.onSingleLineBegins += FirstCard;
+            raycastBlocker.SetActive(true);
+            PayphoneManager.onPhoneStateChanged?.Invoke(true);
+            DOVirtual.DelayedCall(1, () => PayphoneManager.onPhoneMessageSent?.Invoke("First Card")).Play();
+        };
     }
 
     void Start()
     {
         statsTrackingManager = GetComponent<StatsTrackingManager>();
-        isPickedUp = false;
+        isPayphonePickedUp = false;
         isLightOn = false;
+        isCardZoomedForFirstTime = false;
         mask.enabled = true;
 
         Opening(-1);
@@ -52,9 +78,9 @@ public class TutorialManager : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !isPickedUp && isLightOn)
+        if (Input.GetMouseButtonDown(0) && !isPayphonePickedUp && isLightOn)
         {
-            isPickedUp = true;
+            isPayphonePickedUp = true;
             GameManager.Instance.audioManager.StopLoopSound(ringAudioTemp);
             GameManager.Instance.audioManager.PlaySound(payphonePickUpSound);
             PayphoneManager.onPhoneMessageSent?.Invoke("Opening");
@@ -73,6 +99,7 @@ public class TutorialManager : MonoBehaviour
                 {
                     mask.color = Color.clear;
                     isLightOn = true;
+                    GameManager.Instance.audioManager.PlaySound(lightOnSound);
                 });
                 break;
             case 4:
@@ -101,6 +128,35 @@ public class TutorialManager : MonoBehaviour
                 payphoneVolume.enabled = true;
                 foreach (var highlight in toolsHighlights) highlight.enabled = false;
                 HighlightManager.SmoothDisableHighlight(0f);
+                PayphoneManager.onSingleLineBegins -= Opening;
+                break;
+        }
+    }
+
+    void FirstCard(int messageIndex)
+    {
+        switch (messageIndex)
+        {
+            case 2:
+                payphoneVolume.enabled = false;
+                prizeGridHighlight.enabled = true;
+                targetGridHighlights.SetActive(true);
+                HighlightManager.SmoothEnableHighlight(0.5f);
+                break;
+            case 3:
+                prizeGridHighlight.enabled = false;
+                HighlightManager.SmoothDisableEnableCombo(0.5f);
+                break;
+            case 4:
+                prizeGridHighlight.enabled = true;
+                targetGridHighlights.SetActive(false);
+                HighlightManager.SmoothDisableEnableCombo(0.5f);
+                break;
+            case 6:
+                payphoneVolume.enabled = true;
+                prizeGridHighlight.enabled = false;
+                HighlightManager.SmoothDisableHighlight(0f);
+                PayphoneManager.onSingleLineBegins -= FirstCard;
                 break;
         }
     }
