6222c1ee84a5341eb287ad75a84117f1a0dc6d8f
Ziggy-dst <542335894@qq.com>
2024-05-13 14:16:53 -0400

    Unfinished phone automatic play

--- Pre-GDC-prototype-addiction/Assets/_Scripts/PlayerTools/Payphone/ScriptablePayphoneMessages.cs
+++ Pre-GDC-prototype-addiction/Assets/_Scripts/PlayerTools/Payphone/ScriptablePayphoneMessages.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -7,8 +8,15 @@ namespace _Scripts.PlayerTools.Payphone
     [CreateAssetMenu(fileName = "ScriptablePayphoneMessages", menuName = "ScriptableObjects/ScriptablePayphoneMessages", order = 0)]
     public class ScriptablePayphoneMessages : SerializedScriptableObject
     {
+        [Serializable]
+        public struct MessageInfo
+        {
+            public bool playAutomatically;
+            public List<string> messageText;
+        }
+
         [InfoBox("Message name is used to identify messages, message is a list, each element in the list will be shown individually in a text bubble")]
         [DictionaryDrawerSettings(KeyLabel = "Message Name", ValueLabel = "Message Text", DisplayMode = DictionaryDisplayOptions.Foldout)]
-        public Dictionary<string, List<string>> PhoneMessageDict = new();
+        public Dictionary<string, MessageInfo> PhoneMessageDict = new();
     }
 }
